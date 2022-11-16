246549e9baadabf51325cd911d9a37b3b5756b9c
ZhennanRu <zhennan@stanfrod.edu>
2022-11-15 23:22:59 -0800

    milestone project: build up CNN and visualize the result

--- dataset.py
+++ dataset.py
@@ -1,7 +1,27 @@
 # create an interface function to get splited dataset
 # normalization could be added later
 
-def getMFCCDataset():
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import random
+
+# train, valid, test data are all in list consistes of [mfccs, file name]
+# want to change the list to two outputs:
+# X: vector of mfccs
+# Y: vector of labels
+def data2vector(data):
+    X = []
+    Y = []
+    for i in range(len(data)):
+        X.append(data[i][0])
+        Y.append(data[i][1])
+    X = np.array(X)
+    Y = np.array(Y)
+    return X,Y
+
+def getMFCCDataset(train_size = 0.6, valid_size = 0.2, test_size = 0.2):
 
     MFCCs_DATA = "MFCCsData"
     numpy_datas = []
@@ -14,8 +34,7 @@ def getMFCCDataset():
         numpy_datas.extend(datalist)
 
     for i in range(len(numpy_datas)):
-        numpy_datas[i][0] = np.resize(numpy_datas[i][0], (19,512))
-
+        numpy_datas[i][0] = np.transpose(np.resize(numpy_datas[i][0], (19,512)))
     collection = {}
     angry = []
     happy = []
@@ -37,16 +56,34 @@ def getMFCCDataset():
     random.shuffle(happy)
     random.shuffle(normal)
 
-    train_data = angry[:int(len(angry)*0.6)] + happy[:int(len(happy)*0.6)] + normal[:int(len(normal)*0.6)]
-    valid_data = angry[int(len(angry)*0.6):int(len(angry)*(0.6+0.2))] + happy[int(len(happy)*0.6):int(len(happy)*(0.6+0.2))] + normal[int(len(normal)*0.6):int(len(normal)*(0.6+0.2))]
-    test_data = angry[int(len(angry)*(0.6+0.2)):] + happy[int(len(happy)*(0.6+0.2)):] + normal[int(len(normal)*(0.6+0.2)):]
+    train_data = angry[:int(len(angry)*train_size)] + happy[:int(len(happy)*train_size)] + normal[:int(len(normal)*train_size)]
+    valid_data = angry[int(len(angry)*train_size):int(len(angry)*(train_size+valid_size))] + happy[int(len(happy)*train_size):int(len(happy)*(train_size+valid_size))] + normal[int(len(normal)*train_size):int(len(normal)*(train_size+valid_size))]
+    test_data = angry[int(len(angry)*(train_size+valid_size)):] + happy[int(len(happy)*(train_size+valid_size)):] + normal[int(len(normal)*(train_size+valid_size)):]
     random.shuffle(train_data)
     random.shuffle(valid_data)
     random.shuffle(test_data)
 
-    collection['X_train'], collection['Y_train'] = data2vector(train_data)
-    collection['X_valid'], collection['Y_valid'] = data2vector(valid_data)
-    collection['X_test'], collection['Y_test'] = data2vector(test_data)
+    X_train, Y_train = data2vector(train_data)
+    X_valid, Y_valid = data2vector(valid_data)
+    X_test, Y_test = data2vector(test_data)
+
+    collection['X_train'] = X_train
+    collection['Y_train'] = Y_train
+    collection['X_valid'] = X_valid
+    collection['Y_valid'] = Y_valid
+    collection['X_test'] = X_test
+    collection['Y_test'] = Y_test
+    
+    X_train_miu = np.average(X_train, axis=0)
+    X_train_std = np.std(X_train, axis=0)
+
+    X_train_norm = (X_train - X_train_miu)/X_train_std
+    X_valid_norm = (X_valid - X_train_miu)/X_train_std
+    X_test_norm = (X_test - X_train_miu)/X_train_std
+
+    collection['X_train_norm'] = X_train_norm
+    collection['X_valid_norm'] = X_valid_norm
+    collection['X_test_norm'] = X_test_norm
 
     # further add normalization
     return collection
