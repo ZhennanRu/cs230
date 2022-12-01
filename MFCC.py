e275c10204d1f416f233c5cba0a7a7979abf78d5
ZhennanRu <zhennan@stanfrod.edu>
2022-12-01 01:36:39 -0800

    import RAVDESS

--- MFCC.py
+++ MFCC.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa
+import dataset
 # load wave file and sampling frequency
 y, sr = librosa.load('../voice/tsuchiya_normal/tsuchiya_normal_001.wav')
 
 plt.plot(y)
+
+dataset.getMFCCDataset
